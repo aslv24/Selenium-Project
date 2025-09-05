abaa6963911578272d567882edad68e260c77075
aslv24 <aslinfomats@gmail.com>
2025-09-05 14:09:35 +0530

    Selenium Class day 3

---
+++ Selenium Tutorial 1 sep/src/com/selenium/webLevel/AlertHandling.java
@@ -0,0 +1,38 @@
+package com.selenium.webLevel;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AlertHandling {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		
+		WebElement loginButton = driver.findElement(By.className("signin-btn"));
+		loginButton.click();
+		
+		Alert alt = driver.switchTo().alert();
+		
+		System.out.println(alt.getText());
+		
+		alt.accept();
+		
+		
+		
+	}
+
+}
---
+++ Selenium Tutorial 1 sep/src/com/selenium/webLevel/ImplictWait.java
@@ -0,0 +1,42 @@
+package com.selenium.webLevel;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ImplictWait {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://www.facebook.com/");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		
+		WebElement emailField = driver.findElement(By.id("email"));
+		
+		System.out.println(emailField.isDisplayed());//true
+		System.out.println(emailField.isEnabled());//true
+		System.out.println(emailField.isSelected());//false
+		
+		System.out.println(emailField.getTagName());
+		System.out.println(emailField.getAttribute("class"));
+		System.out.println(emailField.getAttribute("placeholder"));
+		
+		emailField.click();
+		emailField.clear();
+		emailField.sendKeys("email@mail.com");
+		
+		System.out.println(emailField.getAttribute("value"));
+		
+	}
+
+}
