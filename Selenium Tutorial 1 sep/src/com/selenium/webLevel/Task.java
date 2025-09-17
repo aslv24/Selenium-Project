6d54102c65e8e1765c309d11e04970d215fb48cf
aslv24 <aslinfomats@gmail.com>
2025-09-17 13:59:00 +0530

    Selenium Class Day 9

--- Selenium Tutorial 1 sep/src/com/selenium/webLevel/Task.java
+++ Selenium Tutorial 1 sep/src/com/selenium/webLevel/Task.java
@@ -1,32 +1,15 @@
 package com.selenium.webLevel;
 
-import java.time.Duration;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Task {
-	
-	static WebDriver driver;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		WebDriver driver = new ChromeDriver();
+		for(int i=6; i<=20;i++)
+		{
+			System.out.println("/html/body/div["+i+"]/div[1]/table/tbody");
+		}
 		
-		driver.manage().window().maximize();
-		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-		
-		driver.get("https://www.google.co.in/");
-		
-		System.out.println(driver.getTitle());
-		
-		System.out.println(driver.getCurrentUrl());
-		
-		WebElement emailfieldElement = driver.findElement(By.xpath(""));
 	}
 
 }
